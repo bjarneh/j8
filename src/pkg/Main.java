a5a531238862c577be96d8e8e3ecb090a0d51792
Bjarne Holen <bjarneholen@gmail.com>
2014-03-25 22:43:33 +0100

    minor: new argument -verbose

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -23,10 +23,11 @@ public class Main {
 
 
     final static String help =
-        "\n jsloc - simple source line counter\n\n"+
-        " usage: jsloc <DIR>                  \n\n"+
-        " options:                            \n\n"+
-        "   -h --help : print this and exit     \n";
+        "\n jsloc - simple source line counter  \n\n"+
+        " usage: jsloc <DIR>                    \n\n"+
+        " options:                              \n\n"+
+        "   -h --help    : print this and exit    \n"+
+        "   -v --verbose : show info for each file\n";
 
 
     public static void main(String[] args)
@@ -34,14 +35,24 @@ public class Main {
     {
 
         List<String> argList     = Arrays.asList( args );
-        Predicate<String> isHelp = 
-            (s) -> s.equals("-h") || s.equals("-help") || s.equals("--help");
+
+        Predicate<String> isHelp = (s) -> s.matches("-(h|-?help)");
+        Predicate<String> isVerbose = (s) -> s.matches("-(v|-?verbose)");
+        Predicate<String> nonWhite = (s) -> !s.matches("^\\s*$");
 
         boolean needsHelp = argList.stream().anyMatch( isHelp );
+        boolean verbose   = argList.stream().anyMatch( isVerbose );
 
         if( needsHelp || argList.isEmpty() ) {
             System.out.println( help );
             System.exit(0);
+        }
+
+        // filter out the -verbose argument
+        if( verbose ){
+            argList = argList.stream()
+                             .filter( isVerbose.negate() )
+                             .collect( Collectors.toList() );
         }
 
         Path path = Paths.get(argList.get(0));
@@ -52,19 +63,33 @@ public class Main {
         }
 
         List<Path> files = Files.walk( path )
+                                .filter( Files::isReadable )
                                 .filter( Files::isRegularFile )
                                 .collect( Collectors.toList() );
 
-        int total = 0;
+        long tmp;
+        long total = 0;
+
         for( Path p: files ) {
+
             if( Files.probeContentType(p).startsWith("text/") ){
-                total += Files.lines(p)
-                              .filter( (s) -> !s.matches("^\\s*$") )
-                              .count();
+
+                tmp = Files.lines(p)
+                           .filter( nonWhite )
+                           .count();
+
+                total += tmp;
+
+                if( verbose ){
+                    System.out.printf(" %7d: %10s : %s\n", tmp,
+                            Files.probeContentType(p), p);
+                }
+
             }
         }
 
-        System.out.printf(" sloc: %s\n", total);
+        System.out.printf(" %7d: sloc\n", total);
+
     }
 
 }
